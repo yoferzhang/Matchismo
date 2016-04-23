894365f42f7122c03fb406d0c4598f2c119cfc0d
yoferzhang <522376829@qq.com>
2016-04-23 10:43:07 +0800

    Initial Commit

---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Yofer Zhang on 16/4/23.
+//  Copyright © 2016年 Yofer Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Matchismo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Yofer Zhang on 16/4/23.
+//  Copyright © 2016年 Yofer Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
