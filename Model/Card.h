baf57229b982d33de3ce72ec036a1be15351c60c
yoferzhang <522376829@qq.com>
2016-04-23 10:53:04 +0800

    try git

---
+++ Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Yofer Zhang on 16/4/23.
+//  Copyright © 2016年 Yofer Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Yofer Zhang on 16/4/23.
+//  Copyright © 2016年 Yofer Zhang. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
